d3c594b677ddfefdbf55c318fdf582b9cd2e28bf
Senthil Kumar <senthilku@Virtusa.com>
2019-01-23 19:20:51 +0530

    First commit

---
+++ WebDriverTest/src/main/java/stepDefinition/Test_Steps.java
@@ -0,0 +1,55 @@
+package stepDefinition;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class Test_Steps {
+	public static WebDriver driver;
+	@Given("^User is on Home Page$")
+	public void user_is_on_Home_Page() throws Throwable {
+		System.setProperty("webdriver.chrome.driver", "C:\\Eclipse\\chromedriver.exe"); // path of chromedriver 
+		driver = new ChromeDriver();
+	    driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+	  
+        driver.get("https://www.google.co.in/");
+        driver.manage().window().maximize();
+        Thread.sleep(10000);
+	}
+ 
+	@When("^User Navigate to LogIn Page$")
+	public void user_Navigate_to_LogIn_Page() throws Throwable {
+		driver.findElement(By.xpath("//a[text()='Gmail']")).click();
+		}
+ 
+	@When("^User enters \"(.*)\" and \"(.*)\"$")
+	public void user_enters_UserName_and_Password(String username, String password) throws Throwable {
+		driver.findElement(By.id("identifierId")).sendKeys(username);
+		driver.findElement(By.xpath("//span[text()='Next']")).click();
+	    driver.findElement(By.name("password")).sendKeys(password);
+	    Thread.sleep(5000);
+	    driver.findElement(By.id("passwordNext")).click();
+		}
+ 
+	@Then("^Message displayed Login Successfully$")
+	public void message_displayed_Login_Successfully() throws Throwable {
+		System.out.println("Login Successfully");
+	}
+ 
+	@When("^User LogOut from the Application$")
+	public void user_LogOut_from_the_Application() throws Throwable {
+		driver.findElement(By.xpath("//a[text()='Sign out']")).click();
+	}
+ 
+	@Then("^Message displayed Logout Successfully$")
+	public void message_displayed_Logout_Successfully() throws Throwable {
+        System.out.println("LogOut Successfully");
+	}
+
+}
